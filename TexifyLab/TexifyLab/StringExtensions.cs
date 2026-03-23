2caca7d2e2c3e71c28cb65db05ecf62e3ec5c894
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2026-03-23 21:21:53 +0700

    Initial source code, for first string extensions

---
+++ TexifyLab/TexifyLab/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace TexifyLab
+{
+    public static class StringExtensions
+    {
+        public static string Intersperse(this string? input, string separator)
+        {
+            ArgumentNullException.ThrowIfNullOrEmpty(separator, nameof(separator));
+
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return input;
+            }
+
+            return string.Join(separator.ToString(), input.ToCharArray());
+        }
+    }
+}
