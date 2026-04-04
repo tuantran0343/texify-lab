1238684decbb67e0c7c5052325fbacced0dd85ad
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2026-04-04 11:32:21 +0700

    add hack 3

--- TexifyLab/ConsoleApp/Program.cs
+++ TexifyLab/ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello world, this is sample text from 012357!");
+Console.WriteLine("Hello world, this is sample text from 0123578!");
 
 Console.ReadKey();
