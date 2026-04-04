d853d40c1729c05ff9fdcd8493583ecf45740000
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2026-04-04 11:36:51 +0700

    log enviroment runner

--- TexifyLab/ConsoleApp/Program.cs
+++ TexifyLab/ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello world, this is sample text from 0123578!");
+Console.WriteLine("Hello world, this is sample text from 01235789!");
 
 Console.ReadKey();
