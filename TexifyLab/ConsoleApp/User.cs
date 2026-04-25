232249536434ab19da51f75df63d21489806c0a6
Tuan Tran <42497135+tuantran0343@users.noreply.github.com>
2026-04-25 17:07:22 +0700

    Update wiremock code

---
+++ TexifyLab/ConsoleApp/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ConsoleApp;
+
+public class User
+{
+    [JsonPropertyName("firstName")]
+    public string FirstName { get; set; }
+
+    [JsonPropertyName("lastName")]
+    public string LastName { get; set; }
+
+    [JsonPropertyName("userName")]
+    public string UserName { get; set; }
+}
